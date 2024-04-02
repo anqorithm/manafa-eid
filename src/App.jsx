3eeb940fd2278e13e9419c68a1a1e647781ca94d
Abdullah Alqahtani <aalqahtani@manafa.co>
2024-04-02 04:16:06 +0300

    fix: fonts & styles bugs

--- src/App.jsx
+++ src/App.jsx
@@ -34,13 +34,11 @@ function App() {
     const ctx = canvas.getContext('2d');
     const image = new Image();
     console.log(i18n.language)
-    image.src = 'src/assets/empty-card-CZooAyTR.png';
+    image.src = 'src/assets/empty-card.png';
     console.log(image.width, image.src)
 
-
     if (document.fonts) {
-      // If Font Loading API is available
-      document.fonts.load('120px NeuePlak').then(function () {
+      document.fonts.load('NeuePlak').then(function () {
         console.log('Font loaded')
       });
     } else {
@@ -51,7 +49,7 @@ function App() {
       canvas.width = image.width;
       canvas.height = image.height;
       ctx.drawImage(image, 0, 0);
-      ctx.font = i18n.language === 'ar' ? '120px Cairo' : '120px Neue Plak';
+      ctx.font = i18n.language === 'ar' ? '120px Cairo' : '120px NeuePlak';
       ctx.textAlign = 'center';
       ctx.fillStyle = 'black';
       console.log(ctx.font)
@@ -76,7 +74,6 @@ function App() {
   const fontClass = isRtl ? 'font-cairo' : 'font-neueplak';
   const direction = isRtl ? 'rtl' : 'ltr';
 
-
   return (
     <div dir={direction} className={`flex flex-col min-h-screen bg-dark-blue-gradient ${fontClass}`}>
       <div className="pt-8 flex-grow">
@@ -86,7 +83,7 @@ function App() {
         <div className="text-white text-sm flex space-x-2 justify-center mt-2 mb-8" dir="ltr">
           <span
             onClick={() => switchLanguage('en')}
-            className={`cursor-pointer font-bold font-neueplak hover:opacity-75 ${i18n.language === 'en' ? 'underline' : ''
+            className={`cursor-pointer pt-0.5 font-bold font-neueplak hover:opacity-75 ${i18n.language === 'en' ? 'underline' : ''
               }`}
           >
             English
@@ -100,17 +97,18 @@ function App() {
             العربية
           </span>
         </div>
-        <p className={`text-xl text-white font-bold text-center mb-8 max-w-md mx-auto`}>{t('message')}</p>
+        <p className={`text-xl text-white text-center mb-8 max-w-md mx-auto`}>{t('message')}</p>
         <div className="flex justify-center items-center">
           <div className="bg-white p-2 shadow-lg rounded-lg max-w-sm w-full">
             <div className="relative" id="image-download">
               <img src={img} alt={t('Eid card background')} className="object-cover rounded-md" />
-              <h1 className={`absolute bottom-[5rem] left-0 right-0 text-center text-xl font-semibold mx-8`}>{text}</h1>          </div>
+              <h1 className={`absolute bottom-[5rem] left-0 right-0 text-center text-xl font-medium mx-8`}>{text}</h1>
+            </div>
             <div className="mt-4">
               <input
                 type="text"
-                dir={direction} // Apply RTL or LTR based on the language
-                className={`w-full p-2 text-md border-2 border-blue-300 rounded-md focus:outline-none focus:border-blue-500 transition-colors duration-300 ease-in-out`}
+                dir={direction}
+                className={`w-full font-medium p-2 text-md border-2 border-blue-300 rounded-md focus:outline-none focus:border-blue-500 transition-colors duration-300 ease-in-out`}
                 placeholder={t('input_name')}
                 onChange={handleText}
                 value={text}
@@ -119,7 +117,7 @@ function App() {
             </div>
             <div className="flex justify-between items-center mt-4">
               <button
-                className={`font-bold bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition-colors duration-300 ease-in-out focus:outline-none w-full`}
+                className={`bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition-colors duration-300 ease-in-out focus:outline-none w-full`}
                 onClick={downloadImageWithText}
               >
                 {t('download')}
